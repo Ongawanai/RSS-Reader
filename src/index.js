d27b1fe310f77b3f94a0e1edf98abf96610eafae
Ongawanai <ssaai@yandex.ru>
2023-04-12 11:54:50 +0500

    Added validation and visual improvments

--- src/index.js
+++ src/index.js
@@ -5,27 +5,6 @@ import * as yup from 'yup';
 import onChange from 'on-change';
 import renderSelector from './renders.js';
 
-const renderStatus = (status) => {
-  const form = document.querySelector('#url-input');
-  const errorText = document.querySelector('.errorText');
-  switch (status) {
-    case true:
-      form.classList.remove('is-invalid');
-      errorText.textContent = '';
-      break;
-    case false:
-      form.classList.add('is-invalid');
-      errorText.textContent = 'Ссылка должна быть валидным URL';
-      break;
-    case 'already exist':
-      form.classList.add('is-invalid');
-      errorText.textContent = 'RSS уже существует';
-      break;
-    default:
-      throw new Error(`invalid form status: ${status}`);
-  }
-};
-
 export default (state) => {
   const watchedState = onChange(state, (path, value) => renderSelector(path, value));
   const inputForm = document.querySelector('.rss-form');
@@ -40,7 +19,6 @@ export default (state) => {
     schema.isValid({ rssInput: input.value }).then((result) => {
       if (result === true) {
         watchedState.formState.feedList.push(input.value);
-        console.log(watchedState.formState.feedList);
         watchedState.formState.isValid = true;
         inputForm.reset();
         input.focus();
