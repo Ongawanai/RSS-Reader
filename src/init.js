2d707da984411d761e2e5869ef4e1649f3054330
Ongawanai <ssaai@yandex.ru>
2023-06-09 12:25:39 +0500

    Delete unused code

--- src/init.js
+++ src/init.js
@@ -19,15 +19,6 @@ const init = () => {
     debug: true,
     resources,
   });
-  yup.setLocale({
-    string: {
-      default: `${i18nextInstance.t('string')}`,
-      url: `${i18nextInstance.t('url')}`,
-    },
-    mixed: {
-      notOneOf: `${i18nextInstance.t('notOneOf')}`,
-    },
-  });
   app(state, i18nextInstance);
 };
 
